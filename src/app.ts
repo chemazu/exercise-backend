4cee589a3c83e0a57fe459452c6cc95cb3c7f304
chemazu <chukwuemekachemazu@gmail.com>
2022-07-22 16:28:27 +0100

    added mongo

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,19 @@
-import express from 'express';
+import express from "express";
+import mongoose from "mongoose";
+import { config } from "dotenv";
 const app = express();
 const port = 4000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+// clg('Hello World');
+config();
+mongoose.connect(
+  process.env.MONGO_URI
+);
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function () {
+  console.log("Connected successfully");
 });
-
 app.listen(port, () => {
   return console.log(`Express is listening at http://localhost:${port}`);
 });
